828aba7a3203d5f2f5053e3a46db2226674f804a
Pedro Marchiori <pmarchiori08@gmail.com>
2025-06-26 15:09:31 -0300

    creation of the routes system

--- src/components/DashboardItem.js
+++ src/components/DashboardItem.js
@@ -1,4 +1,4 @@
-export function DashboardItem({ title, description = "" }) {
+export function DashboardItem({ title, description = "", onClick }) {
   const dashboardItem = document.createElement("div");
   dashboardItem.classList.add("dashboard-item");
 
@@ -8,11 +8,19 @@ export function DashboardItem({ title, description = "" }) {
 
   dashboardItem.appendChild(dashboardItemTitle);
 
-  if (dashboardItem) {
+  if (description) {
     const dashboardItemDescription = document.createElement("a");
     dashboardItemDescription.textContent = description;
     dashboardItemDescription.classList.add("textMd");
     dashboardItemDescription.style.color = "var(--indigo-700)";
+    dashboardItemDescription.href = "#";
+
+    if (typeof onClick === "function") {
+      dashboardItemDescription.addEventListener("click", (e) => {
+        e.preventDefault();
+        onClick();
+      });
+    }
 
     dashboardItem.appendChild(dashboardItemDescription);
   }
